9ac718457fa5867537eb50dc2800501524481759
EGORIKABOBIK <egornikitin20102009@gmail.com>
2026-07-31 22:00:28 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const DEFAULT_SUPABASE_URL = "https://stckujfyiqldjymnjko.supabase.co";
+export const DEFAULT_SUPABASE_URL = "https://stckujfyqilqdiymmjko.supabase.co";
 export const DEFAULT_SUPABASE_KEY = "sb_publishable_1NUqoZv6jgANO0u62qYGjw_X6aFQ6Ft";
